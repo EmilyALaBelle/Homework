25b64905cc994db29e08c96a8a9ff4bac90d2a63
EmilyALaBelle <emilylabelle8@gmail.com>
2022-10-06 18:29:08 -0400

    h update

--- day4-homework.js
+++ day4-homework.js
@@ -1,10 +1,10 @@
 
-// for(let i = 10; i > 0; i--){
-//     console.log(i)
-//     if(i <= 1){
-//         console.log('BLAST OFF!')
-//     }
-// }
+for(let i = 10; i > 0; i--){
+    console.log(i)
+    if(i <= 1){
+        console.log('BLAST OFF!')
+    }
+}
 
 function rocketShip(counter){
     for(let i = counter; i > 0; i--){
