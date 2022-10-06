260ae06ec8714348f99aea2f3565ab634a6b7812
EmilyALaBelle <emilylabelle8@gmail.com>
2022-10-06 16:02:33 -0400

    homework 3 update

--- day3-homework.js
+++ day3-homework.js
@@ -9,18 +9,18 @@ for (let i = 1; i <= numberOfHashes; i++) {
 /*Write a function named assignGrade that takes one argument, a number score.
 Returns a grade for the score, either  "A," "B," "C," "D," or "F." Call that 
 function for a few different scores and log the result to make sure it works.*/
-const score = 60
+
 function assignGrade(numScore) {
-    if (score >= 90) {
+    if (numScore >= 90) {
         console.log('A')
-    } else if (score >= 80) {
+    } else if (numScore >= 80) {
         console.log('B')
-    } else if (score >= 70) {
+    } else if (numScore >= 70) {
         console.log('C')
-    } else if (score >= 60) {
+    } else if (numScore >= 60) {
         console.log('D')
     } else{
         console.log('F')
     }
 }
-assignGrade(score)
+assignGrade(90)
