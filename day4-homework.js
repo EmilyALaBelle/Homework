6cdd41d23b91a15371149eff3d04b0814f116dfb
EmilyALaBelle <emilylabelle8@gmail.com>
2022-10-06 16:12:33 -0400

    blast off

---
+++ day4-homework.js
@@ -0,0 +1,17 @@
+
+// for(let i = 10; i > 0; i--){
+//     console.log(i)
+//     if(i <= 1){
+//         console.log('BLAST OFF!')
+//     }
+// }
+
+function rocketShip(counter){
+    for(let i = counter; i > 0; i--){
+        console.log(i)
+        if(i <= 1){
+            console.log('BLAST OFF!')
+        }
+    } 
+}
+rocketShip(21)
